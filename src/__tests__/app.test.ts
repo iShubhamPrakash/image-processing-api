73eddf1b7a5c47a11ac66acbc7d5845e5ecc70ed
i-shubhamprakash <shubham.prakash2308@gmail.com>
2021-02-01 07:09:51 +0530

    Add unit tests for /resize endpoint

--- src/__tests__/app.test.ts
+++ src/__tests__/app.test.ts
@@ -10,7 +10,34 @@ describe('GET /test - a test api endpoint', () => {
 
 describe('Test / route', () => {
   it("Request to '/' should return staus 200", async () => {
-    const result = await request(app).get('/').send();
+    const result = await request(app).get('/');
     expect(result.status).toBe(200);
   });
+});
+
+describe('Test /resize route', () => {
+  it('Request to /resize should return staus 404 for an unknown file', async () => {
+    const result = await request(app)
+      .get('/resize')
+      .query({
+        name: 'thisisarandomfilename',
+        width: 200,
+        height: 200
+      })
+      .send();
+    expect(result.status).toBe(404);
+  });
+
+  it('Request to /resize should return staus 200 for a known file', async () => {
+    const result = await request(app)
+      .get('/resize')
+      .query({
+        name: 'fjord',
+        width: 200,
+        height: 200
+      })
+      .send();
+    expect(result.status).toBe(200);
+  });
+
 });
